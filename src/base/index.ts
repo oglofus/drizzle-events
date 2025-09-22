a73650426ebe3d3b2c2a22afa8a20ee608919cf7
Nikolaos Grammatikos <nikosgram@oglofus.com>
2025-09-22 20:21:13 +0100

    Initial project setup with SQLite event management functionality.
    - Added `.idea` IDE configuration.
    - Created SQLite-based event manager with pre/post event handling for CRUD operations.
    - Integrated dependencies such as `@oglofus/event-manager` and `drizzle-orm`.
    - Defined TypeScript configurations and `package.json`.
    - Added GitHub workflow for NPM package release.
    - Updated `.gitignore` to exclude common project files.

---
+++ src/base/index.ts
@@ -0,0 +1,105 @@
+export type Response<T> =
+    | {
+    type: 'success';
+    data: T;
+}
+    | {
+    type: 'error';
+    message?: string | undefined;
+};
+
+export type ArrayStrategy =
+    'replace'
+    | 'concat'
+    | 'union';
+
+export type EventManagerConfig = {
+    merge_objects: boolean;
+    array_strategy: ArrayStrategy;
+    rollback_on_cancel: boolean;
+}
+
+export type PartialEventManagerConfig = Partial<EventManagerConfig>;
+
+export const DefaultEventManagerConfig: EventManagerConfig = {
+    merge_objects: true,
+    array_strategy: 'union',
+    rollback_on_cancel: true
+};
+
+export type Primitive =
+    string
+    | number
+    | boolean
+    | symbol
+    | bigint
+    | null
+    | undefined;
+
+export type IsPlainObject<T> = T extends Primitive ? false : T extends Function
+                                                             ? false
+                                                             : T extends readonly any[]
+                                                               ? false
+                                                               : T extends object ? true : false;
+
+export type MergeDeep<T, U> = {
+    [K in keyof T | keyof U]: K extends keyof U ? K extends keyof T
+                                                  ? IsPlainObject<T[K]> extends true
+                                                    ? IsPlainObject<U[K]> extends true
+                                                      ? MergeDeep<T[K], U[K]>
+                                                      : U[K]
+                                                    : U[K]
+                                                  : U[K] : K extends keyof T ? T[K] : never;
+};
+
+
+export const isPlainObject = (value: unknown): value is Record<string, unknown> => Object.prototype.toString.call(
+    value) === '[object Object]';
+
+export function deepMerge<T, U>(
+    a: T,
+    b: U,
+    array_strategy: ArrayStrategy = "replace",
+): MergeDeep<T, U> {
+    if (!isPlainObject(a)) {
+        return b as MergeDeep<T, U>;
+    }
+
+    if (!isPlainObject(b)) {
+        return b as MergeDeep<T, U>;
+    }
+
+    const out: Record<string, unknown> = {...a as Record<string, unknown>};
+
+    for (const [key, b_value] of Object.entries(b)) {
+        const a_value = (a as Record<string, unknown>)[key];
+
+        if (isPlainObject(a_value) && isPlainObject(b_value)) {
+            out[key] = deepMerge(a_value, b_value, array_strategy);
+
+            continue;
+        }
+
+        if (Array.isArray(a_value) && Array.isArray(b_value)) {
+            if (array_strategy === 'concat') {
+                out[key] = [...a_value, ...b_value];
+            } else if (array_strategy === 'union') {
+                out[key] = Array.from(new Set([...a_value, ...b_value]));
+            } else {
+                out[key] = [...b_value];
+            }
+
+            continue;
+        }
+
+        if (Array.isArray(b_value)) {
+            out[key] = [...b_value];
+        } else if (isPlainObject(b_value)) {
+            out[key] = {...b_value};
+        } else {
+            out[key] = b_value;
+        }
+    }
+
+    return out as MergeDeep<T, U>;
+}
---
+++ src/sqlite/index.ts
@@ -0,0 +1,406 @@
+import {
+    eq,
+    getTableColumns,
+    getTableUniqueName,
+    InferInsertModel,
+    InferSelectModel
+} from "drizzle-orm";
+import {CancellableEvent, EventPriority, RawEventManager} from "@oglofus/event-manager";
+import {
+    BaseSQLiteDatabase,
+    SQLiteTableWithColumns,
+    SQLiteUpdateSetSource
+} from "drizzle-orm/sqlite-core";
+import {
+    deepMerge,
+    DefaultEventManagerConfig,
+    EventManagerConfig,
+    PartialEventManagerConfig,
+    Response
+} from "../base/index.js";
+
+export class SQLitePreInsertEvent<T extends SQLiteTableWithColumns<any>> extends CancellableEvent {
+    private readonly _data: InferInsertModel<T>;
+
+    constructor(data: InferInsertModel<T>) {
+        super();
+
+        this._data = data;
+    }
+
+    get data(): InferInsertModel<T> {
+        return this._data;
+    }
+}
+
+export class SQLitePostInsertEvent<T extends SQLiteTableWithColumns<any>> extends CancellableEvent {
+    private readonly _row: InferSelectModel<T>;
+
+    constructor(data: InferSelectModel<T>) {
+        super();
+
+        this._row = data;
+    }
+
+    get row(): InferSelectModel<T> {
+        return this._row;
+    }
+}
+
+export class SQLitePreUpdateEvent<T extends SQLiteTableWithColumns<any>> extends CancellableEvent {
+    private readonly _data: SQLiteUpdateSetSource<T>;
+    private readonly _row: InferSelectModel<T>;
+
+    constructor(data: SQLiteUpdateSetSource<T>, row: InferSelectModel<T>) {
+        super();
+
+        this._data = data;
+        this._row = row;
+    }
+
+    get data(): SQLiteUpdateSetSource<T> {
+        return this._data;
+    }
+
+    get row(): InferSelectModel<T> {
+        return this._row;
+    }
+}
+
+export class SQLitePostUpdateEvent<T extends SQLiteTableWithColumns<any>> extends CancellableEvent {
+    private readonly _row: InferSelectModel<T>;
+    private readonly _old_row: InferSelectModel<T>;
+
+    constructor(row: InferSelectModel<T>, old_row: InferSelectModel<T>) {
+        super();
+
+        this._row = row;
+        this._old_row = old_row;
+    }
+
+    get row(): InferSelectModel<T> {
+        return this._row;
+    }
+
+    get old_row(): InferSelectModel<T> {
+        return this._old_row;
+    }
+}
+
+export class SQLitePreDeleteEvent<T extends SQLiteTableWithColumns<any>> extends CancellableEvent {
+    private readonly _row: InferSelectModel<T>;
+
+    constructor(row: InferSelectModel<T>) {
+        super();
+
+        this._row = row;
+    }
+
+    get row(): InferSelectModel<T> {
+        return this._row;
+    }
+}
+
+export class SQLitePostDeleteEvent<T extends SQLiteTableWithColumns<any>> extends CancellableEvent {
+    private readonly _row: InferSelectModel<T>;
+
+    constructor(row: InferSelectModel<T>) {
+        super();
+
+        this._row = row;
+    }
+
+    get row(): InferSelectModel<T> {
+        return this._row;
+    }
+}
+
+type SQLiteEventType =
+    | 'pre-insert'
+    | 'post-insert'
+    | 'pre-update'
+    | 'post-update'
+    | 'pre-delete'
+    | 'post-delete';
+
+type SQLiteEventClass<T extends SQLiteTableWithColumns<any>, E extends SQLiteEventType> = E extends 'pre-insert'
+                                                                                          ? SQLitePreInsertEvent<T>
+                                                                                          : E extends 'post-insert'
+                                                                                            ? SQLitePostInsertEvent<T>
+                                                                                            : E extends 'pre-update'
+                                                                                              ? SQLitePreUpdateEvent<T>
+                                                                                              : E extends 'post-update'
+                                                                                                ? SQLitePostUpdateEvent<T>
+                                                                                                : E extends 'pre-delete'
+                                                                                                  ? SQLitePreDeleteEvent<T>
+                                                                                                  : SQLitePostDeleteEvent<T>;
+
+export class SQLiteEventManager<TSchema extends Record<string, unknown> = Record<string, never>, DSchema extends BaseSQLiteDatabase<any, any, TSchema> = BaseSQLiteDatabase<any, any, TSchema>>
+    extends RawEventManager {
+    private readonly _config: EventManagerConfig;
+    private readonly _database: DSchema;
+
+    constructor(database: DSchema, config?: PartialEventManagerConfig) {
+        super();
+        this._database = database;
+        this._config = deepMerge(DefaultEventManagerConfig, config ?? {});
+    }
+
+    get database(): DSchema {
+        return this._database;
+    }
+
+    get config(): EventManagerConfig {
+        return this._config;
+    }
+
+    public async insert<T extends SQLiteTableWithColumns<any>>(
+        table: T,
+        primary_field: keyof InferSelectModel<T>,
+        data: InferInsertModel<T>
+    ): Promise<Response<InferSelectModel<T>>> {
+        const pre_response = await this.run(table, 'pre-insert', new SQLitePreInsertEvent<T>(data));
+
+        if (pre_response.event.isCancelled()) {
+            return {
+                type: 'error',
+                message: pre_response.event.getCancelReason()
+            };
+        }
+
+        data = pre_response.event.data;
+        let results: InferSelectModel<T>[];
+
+        try {
+            results = (await this._database
+                                 .insert(table)
+                                 .values(data)
+                                 .returning()) as InferSelectModel<T>[];
+        } catch (error) {
+            return {
+                type: 'error',
+                message: 'An error occurred while inserting the data.'
+            };
+        }
+
+        if (results.length === 0) {
+            return {
+                type: 'error',
+                message: 'An error occurred while inserting the data.'
+            };
+        }
+
+        const row = results[0];
+
+        const post_response = await this.run(
+            table,
+            'post-insert',
+            new SQLitePostInsertEvent<T>(row)
+        );
+
+        if (post_response.event.isCancelled()) {
+            if (this._config.rollback_on_cancel) {
+                await this._database
+                          .delete(table)
+                          .where(eq(table[primary_field], row[primary_field]))
+                          .execute();
+            }
+
+            return {
+                type: 'error',
+                message: post_response.event.getCancelReason()
+            };
+        }
+
+        return {
+            type: 'success',
+            data: row
+        };
+    }
+
+    public async update<T extends SQLiteTableWithColumns<any>>(
+        table: T,
+        primary_field: keyof InferSelectModel<T>,
+        primary_value: typeof primary_field extends keyof InferSelectModel<T>
+                       ? InferSelectModel<T>[keyof InferSelectModel<T>]
+                       : never,
+        data: SQLiteUpdateSetSource<T>
+    ): Promise<Response<InferSelectModel<T>>> {
+        const old_row = await this._database
+                                  .select({
+                                      ...getTableColumns(table)
+                                  })
+                                  .from(table)
+                                  .where(eq(table[primary_field], primary_value))
+                                  .get();
+
+        if (!old_row) {
+            return {
+                type: 'error',
+                message: 'The row does not exist.'
+            };
+        }
+
+        const pre_response = await this.run(
+            table,
+            'pre-update',
+            new SQLitePreUpdateEvent<T>(data, old_row)
+        );
+
+        if (pre_response.event.isCancelled()) {
+            return {
+                type: 'error',
+                message: pre_response.event.getCancelReason()
+            };
+        }
+
+        data = pre_response.event.data;
+
+        if (this._config.merge_objects) {
+            for (const key in old_row) {
+                if (!data[key]) {
+                    continue;
+                }
+
+                // @ts-ignore
+                data[key] = deepMerge(old_row[key], data[key])
+            }
+        }
+
+        let results: InferSelectModel<T>[];
+
+        try {
+            results = (await this._database
+                                 .update(table)
+                                 .set(data)
+                                 .where(eq(table[primary_field], primary_value))
+                                 .returning()) as InferSelectModel<T>[];
+        } catch (error) {
+            return {
+                type: 'error',
+                message: 'An error occurred while updating the data.'
+            };
+        }
+
+        if (results.length === 0) {
+            return {
+                type: 'error',
+                message: 'An error occurred while updating the data.'
+            };
+        }
+
+        const row = results[0];
+
+        const post_response = await this.run(
+            table,
+            'post-update',
+            new SQLitePostUpdateEvent<T>(row, old_row)
+        );
+
+        if (post_response.event.isCancelled()) {
+            if (this._config.rollback_on_cancel) {
+                await this._database
+                          .update(table)
+                          .set(old_row)
+                          .where(eq(table[primary_field], primary_value))
+                          .execute();
+            }
+
+            return {
+                type: 'error',
+                message: post_response.event.getCancelReason()
+            };
+        }
+
+        return {
+            type: 'success',
+            data: row
+        };
+    }
+
+    public async delete<T extends SQLiteTableWithColumns<any>>(
+        table: T,
+        primary_field: keyof InferSelectModel<T>,
+        primary_value: typeof primary_field extends keyof InferSelectModel<T>
+                       ? InferSelectModel<T>[keyof InferSelectModel<T>]
+                       : never
+    ): Promise<Response<InferSelectModel<T>>> {
+        const row = await this._database
+                              .select({
+                                  ...getTableColumns(table)
+                              })
+                              .from(table)
+                              .where(eq(table[primary_field], primary_value))
+                              .get();
+
+        if (!row) {
+            return {
+                type: 'error',
+                message: 'The row does not exist.'
+            };
+        }
+
+        const pre_response = await this.run(table, 'pre-delete', new SQLitePreDeleteEvent<T>(row));
+
+        if (pre_response.event.isCancelled()) {
+            return {
+                type: 'error',
+                message: pre_response.event.getCancelReason()
+            };
+        }
+
+        try {
+            await this._database.delete(table).where(eq(table[primary_field], primary_value));
+        } catch (error) {
+            return {
+                type: 'error',
+                message: 'An error occurred while updating the data.'
+            };
+        }
+
+        const post_response = await this.run(
+            table,
+            'post-delete',
+            new SQLitePostDeleteEvent<T>(row)
+        );
+
+        if (post_response.event.isCancelled()) {
+            if (this._config.rollback_on_cancel) {
+                await this._database.insert(table).values(row).execute();
+            }
+
+            return {
+                type: 'error',
+                message: post_response.event.getCancelReason()
+            };
+        }
+
+        return {
+            type: 'success',
+            data: row
+        };
+    }
+
+    public put<TKey extends keyof DSchema["_"]["schema"], TTable extends DSchema["_"]["schema"][TKey] & SQLiteTableWithColumns<any>, E extends SQLiteEventType, C extends SQLiteEventClass<TTable, E>>(
+        table: TTable,
+        type: E,
+        handler: (event: C) => Promise<void> | void,
+        priority: EventPriority = EventPriority.NORMAL
+    )
+    {
+        return this._register(this.getEventKey(table, type), handler, priority);
+    }
+
+    public async run<T extends SQLiteTableWithColumns<any>, E extends SQLiteEventType, C extends SQLiteEventClass<T, E>>(
+        table: T,
+        type: E,
+        event: C
+    )
+    {
+        return await this._emit(this.getEventKey(table, type), event);
+    }
+
+    private getEventKey(table: SQLiteTableWithColumns<any>, type: SQLiteEventType) {
+        return getTableUniqueName(table) + ':' + type;
+    }
+}
